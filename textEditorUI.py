c94e7c9256c6585ce97387b8a3d9ff6b2310e1bf
whelanworkmaster <christianworkmaster@gmail.com>
2017-03-03 14:26:49 -0500

    tk Save working
    Implemented a save button to save files, need to implement open next I
    think

--- textEditorUI.py
+++ textEditorUI.py
@@ -1,7 +1,21 @@
 import tkinter
+import tkinter.filedialog
+
+def saveFunc():
+	global text
+
+	t = text.get("1.0", "end-1c")
+	saveFileName = tkinter.filedialog.asksaveasfilename()
+	file = open(saveFileName, "w+")
+	file.write(t)
+	file.close()
 
 top = tkinter.Tk()
 
 text = tkinter.Text(top)
 text.grid()
+
+button = tkinter.Button(top, text = "Save", command = saveFunc)
+button.grid()
+
 top.mainloop()
